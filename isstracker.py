674566c7019991616dce0e3ca88c2ea9f131bca2
KungFoodie <williamxsung@gmail.com>
2023-08-15 19:45:44 +0000

    GET VS POST API

--- isstracker.py
+++ isstracker.py
@@ -4,8 +4,9 @@ import requests
 import datetime
 import reverse_geocoder as rg
 
-
 URL= "http://api.open-notify.org/iss-now.json"
+
+
 def main():
     resp= requests.get(URL).json()
     timestamp = resp['timestamp']
